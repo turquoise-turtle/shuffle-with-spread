da19b8719660214d0bb412a1efc5f65d21be3927
turquoise-turtle <16552560+turquoise-turtle@users.noreply.github.com>
2019-07-06 14:03:24 +1000

    Fixed logic

--- shuffle.js
+++ shuffle.js
@@ -1,23 +1,29 @@
 var l = console.log;
 //https://30secondsofcode.org/object#deepclone
 const deepClone = obj => {
-  let clone = Object.assign({}, obj);
-  Object.keys(clone).forEach(
-    key => (clone[key] = typeof obj[key] === 'object' ? deepClone(obj[key]) : obj[key])
-  );
-  return Array.isArray(obj) && obj.length
-    ? (clone.length = obj.length) && Array.from(clone)
-    : Array.isArray(obj)
-      ? Array.from(obj)
-      : clone;
+	let clone = Object.assign({}, obj);
+	Object.keys(clone).forEach(
+		key => (clone[key] = typeof obj[key] === 'object' ? deepClone(obj[key]) : obj[key])
+	);
+	return Array.isArray(obj) && obj.length
+		? (clone.length = obj.length) && Array.from(clone)
+		: Array.isArray(obj)
+			? Array.from(obj)
+			: clone;
+};
+//based off of https://davidwalsh.name/fill-array-javascript
+var fillRange = function fillRange(start, end) {
+	return Array(end - start + 1).fill().map(function (item, index) {
+		return start + index;
+	});
 };
 
 
 
 var list = {
-	'A': 3,
-	'B': 8,
-	'C': 5
+	'A': 9,
+	'B': 18,
+	'C': 12
 };
 
 var result = spreadShuffle(list);
@@ -35,50 +41,71 @@ function spreadShuffle(meta) {
 	var newMeta = deepClone(meta);
 	Object.keys(newMeta).forEach(function(item) {
 		if (newMeta[item] === longest) {
-			//newMeta[item] = [].fill(item, 0, longest);
 			newMeta[item] = Array(longest).fill(item);
 		} else {
-			l(' ')
 			var finalArray = [];
-			//need to get to longest, from current
-			var n = longest;
-			var current = newMeta[item];
-			var dummies = longest - current;
-			var k = current > dummies ? dummies : current;
 			
-			while (n>0) {
-				var newdummies = n - current;
+			var current = newMeta[item];
+			var dummiesleft = longest - current;
+			var actual = item;
+			var dummy = 'Z';
+			//handle the alternate case
+			if (dummiesleft < current) {
+				current = dummiesleft;
+				dummiesleft = newMeta[item];
+				actual = 'Z';
+				dummy = item;
+			}
+			
+			while (dummiesleft > 0) {
+				var toadd = Math.round(dummiesleft / current);
+				finalArray.push(actual);
+				finalArray = finalArray.concat(Array(toadd).fill(dummy));
 				
-				var r = Math.round(n / k);
-				l(n,k,r)
-				l(n - k + 1)
-				/*if (r == 0) {
-					r = 1;
-				}*/
-				/*while (r > (n - k + 1)) {
-					r = r-1;
-					l(r)
-				}*/
-				l(k, newdummies,n,current)
-				if (k == newdummies) {
-					//l(k)
-					l(r)
-					finalArray.push(item);
-					finalArray.concat(Array(r).fill('Z'));
-				} else {
-					//l(k, dummies)
-					l(r)
-					finalArray.push('Z');
-					finalArray.concat(Array(r).fill(item));
-				}
-				k = k - 1;
-				n = n - r;
+				dummiesleft = dummiesleft - toadd;
+				current = current - 1;
 			}
 			newMeta[item] = finalArray;
-			l(' ')
 		}
 	});
+	l(meta);
 	l(newMeta)
+	
+	var rawArray = [];
+	
+	var keys = Object.keys(newMeta);
+	var keyArray = fillRange(0,keys.length-1);
+	
+	//l(keys, keyArray)
+	
+	for (var i=0; i < longest; i++) {
+		//newMeta[keys[0]][i]
+		var currentKeyArray = deepClone(keyArray);
+		//l(currentKeyArray, keyArray)
+		while (currentKeyArray.length > 0) {
+			//l(currentKeyArray, currentKeyArray[0])
+			var randomIndex = Math.floor(Math.random() * currentKeyArray.length);
+			var randomKey = currentKeyArray[randomIndex];
+			//newMeta[keys[randomKey]][i]
+			//l(randomKey, randomIndex, currentKeyArray, keys)
+			rawArray.push(newMeta[keys[randomKey]][i]);
+			currentKeyArray.slice(randomIndex, 1);
+		}
+	}
+	l(rawArray);
+	
+	var processed = rawArray.filter(function(el) {
+		return el !== 'Z';
+	});
+	
+	l(processed)
+	
+	var ul = document.querySelector('ul');
+	for (var el of processed) {
+		var li = document.createElement('li');
+		li.innerText = el;
+		ul.appendChild(li);
+	}
 }
 
 
